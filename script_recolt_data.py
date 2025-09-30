73e9d65d18fff5348b5683d373dc3dc896b28681
Guillaume Reynier <guillaumereynier@outlook.fr>
2025-09-30 16:15:37 +0200

    Transform

--- script_recolt_data.py
+++ script_recolt_data.py
@@ -1,9 +1,7 @@
 import requests
 import json
 import pandas as pd
-import pytz
 import time
-
 
 api_key = "ba66e04fc0c6389242d8598aeee0906fe4b3d805"
 url = f"https://api.jcdecaux.com/vls/v1/stations?contract=toulouse&apiKey={api_key}"
@@ -11,15 +9,13 @@ url = f"https://api.jcdecaux.com/vls/v1/stations?contract=toulouse&apiKey={api_k
 df_all = pd.DataFrame()
 i=0
 
-while i<60:
-    # Requête à l'API
+while i<20:
     response = requests.get(url)
     data = response.json()
-    
-    df = pd.DataFrame(data)
-    
-    df['datetime'] = pd.to_datetime(df['last_update'], unit='ms',)
-    df['datetime'] = df['datetime'].dt.tz_localize('UTC').dt.tz_convert('Etc/GMT-2')
+    df = pd.json_normalize(data)
+
+    df['datetime'] = pd.to_datetime(df['last_update'], unit='ms', utc=True)
+    df['datetime'] = df['datetime'].dt.tz_convert('Etc/GMT-2')
     
     df['fetch_time'] = pd.Timestamp.now()
     
@@ -27,10 +23,12 @@ while i<60:
     
     print(df[['name', 'available_bikes', 'available_bike_stands', 'datetime']].tail(5))
     
-    # Attendre 1 minutes
     i+=1
     time.sleep(60)
 
 
+df_all.to_csv("Extract_test_30_09_16h00.csv")
 
-df_all.to_csv("Extract_test_30_09_15h30.csv")
+
+
+
