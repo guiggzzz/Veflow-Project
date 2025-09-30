7f3400dd735aa7c56e8a41d4dfc53f4a1bfd914b
william94000schr <william.schneider94000@gmail.com>
2025-09-30 16:09:54 +0200

    extract et main

---
+++ main.py
@@ -0,0 +1,22 @@
+from extract_data import extract
+import json
+
+
+def main():
+    # Configuration
+    API_KEY = "ba66e04fc0c6389242d8598aeee0906fe4b3d805"
+    BASE_URL = f"https://api.jcdecaux.com/vls/v1/stations?contract=toulouse&apiKey={API_KEY}"
+    
+    # Extraction
+    print("Démarrage de l'ETL\n")
+    print("Extraction des données depuis l'API\n")
+    data = extract(BASE_URL, API_KEY)
+    
+    #optionnel, juste pour les tests on print les data
+    print(f"\nAperçu des données:\n")
+    print(json.dumps(data[:2], indent=4, ensure_ascii=False))
+
+
+
+if __name__ == "__main__":
+    main()
